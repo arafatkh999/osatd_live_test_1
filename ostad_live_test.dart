329ce94cb3110e8fad304117df97768c7ed5284f
arafatkh999 <arafatkh999@gmail.com>
2023-12-11 23:34:43 +0600

    final version

--- ostad_live_test.dart
+++ ostad_live_test.dart
@@ -1,3 +1,35 @@
-void main (){
+void main() {
+  List<Map<String, dynamic>> fruits = [
+    {'name': 'Apple', 'color': 'Red', 'price': 2.5},
+    {'name': 'Banana', 'color': 'Yellow', 'price': 1.0},
+    {'name': 'Grapes', 'color': 'Purple', 'price': 3.0},
+  ];
+
   
+  displayFruitDetails(fruits);
+
+  
+  applyPriceDiscount(fruits, 10);
+
+  
+  displayFruitDetails(fruits);
+}
+
+
+void displayFruitDetails(List<Map<String, dynamic>> fruits) {
+  print('Fruit Details:');
+  for (var fruit in fruits) {
+    print('Name: ${fruit['name']}, Color: ${fruit['color']}, Price: \$${fruit['price']}');
+  }
+  print('\n');
+}
+
+
+void applyPriceDiscount(List<Map<String, dynamic>> fruits, double discountPercentage) {
+  for (var fruit in fruits) {
+    double originalPrice = fruit['price'];
+    double discountAmount = (originalPrice * discountPercentage) / 100;
+    fruit['price'] = originalPrice - discountAmount;
+  }
+  print('Discount of ${discountPercentage}% applied to fruit prices.\n');
 }
